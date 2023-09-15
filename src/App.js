8ff7e3cad6c0870d21ce094364a130b5596af6d7
Suman Saurabh Das <dsumansaurabh@gmail.com>
2023-09-15 22:35:57 +0530

    01 02 Introduction to render

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+/* -------------------- Section 01 -------------------- */
+
 function App() {
   return (
     <div className="App">
       <h1>React JS Render - Codevolution</h1>
+
+      {/* -------------------- Section 01 -------------------- */}
+      <>
+        
+      </>
     </div>
   );
 }
